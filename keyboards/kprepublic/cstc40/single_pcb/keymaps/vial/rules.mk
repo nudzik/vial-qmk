4e0fcc11fc642ab9fb0608b872aec95d37cc7bad
Jaroslav T <jaroslavt@gmail.com>
2023-09-25 13:01:00 +0000

    cstc40 commit

---
+++ keyboards/kprepublic/cstc40/single_pcb/keymaps/vial/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+SRC += features/layer_lock.c
---
+++ keyboards/kprepublic/cstc40/single_pcb/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+LTO_ENABLE = no
+TRI_LAYER_ENABLE = yes
+#NKRO_ENABLE = yes
+#CONSOLE_ENABLE = yes
+#SRC += features/layer_lock.c
+KEY_OVERRIDE_ENABLE = yes
